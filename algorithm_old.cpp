ccaf3b1a6fd9d7f121e9e3e5c15567f223902e01
Andrii Volynets <116294957+AndryVolynets@users.noreply.github.com>
2023-04-22 16:40:45 +0300

    Update algorithm_old.cpp

--- algorithm_old.cpp
+++ algorithm_old.cpp
@@ -2,56 +2,40 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-#include <cassert>
-#include <sstream>
 
 using namespace std;
 
 int n;
 vector<int> a;
-int dp[50][50];
 
-int dfs(int idx, int removed) {
-
+int dfs(int idx, int cur_sum, int cur_count, int removed, vector<vector<vector<int>>>& dp) {
     if (idx == n) {
-        return removed;
-    }
-    if (dp[idx][removed] != -1) {
-        return dp[idx][removed];
-    }
-    else {
-        int res = dfs(idx + 1, removed);
-
-        for (int i = idx + 1; i < n; i++) {
-
-            // Вычисляем среднее арифметическое первой половины массива
-            double first_half_mean = (double)(accumulate(a.begin() + idx, a.begin() + i, 0)) / (i - idx);
-
-            // Вычисляем среднее арифметическое второй половины массива
-            double second_half_mean = (double)(accumulate(a.begin() + i, a.end(), 0)) / (n - (i - idx));
-
-            // Проверяем, равны ли средние арифметические
-            if (first_half_mean == second_half_mean) {
-                res = max(res, dfs(i + 1, removed + i - idx));
-            }
-
+        if (cur_count > 0 && (double)cur_sum / cur_count == (double)(accumulate(a.begin(), a.end(), 0) - cur_sum) / (n - cur_count)) {
+            return removed;
         }
-        dp[idx][removed] = res;
-        return res;
+        return 0;
     }
+    if (cur_sum > accumulate(a.begin(), a.end(), 0) || cur_count > n) {
+        return -1;
+    }
+    if (dp[idx][cur_sum][cur_count] != -1) {
+        return dp[idx][cur_sum][cur_count] + removed;
+    }
+    int res = dfs(idx + 1, cur_sum + a[idx], cur_count + 1, removed, dp);
+    res = max(res, dfs(idx + 1, cur_sum, cur_count, removed + 1, dp));
+    dp[idx][cur_sum][cur_count] = res - removed;
+    return res;
 }
 
 int main() {
-    // Считываем входные данные.
     cin >> n;
     a.resize(n);
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
 
-    fill(&dp[0][0], &dp[0][0] + sizeof(dp) / sizeof(int), -1);
-
-    cout << dfs(0, 0) << endl;
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(accumulate(a.begin(), a.end(), 0) + 1, vector<int>(n + 1, -1)));
+    cout << dfs(0, 0, 0, 0, dp) << endl;
 
     return 0;
 }
